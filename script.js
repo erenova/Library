c4e46390bf49124138a1abd1c894204a8af6fa7c
erenova <erenova6@gmail.com>
2023-08-16 07:37:34 +0300

    add Book constructor

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
-const domItems = {
+const bookLibrary = [];
+
+function Book(name, author, totalPage, state = false) {
+  this.name = name;
+  this.author = author;
+  this.totalPage = totalPage;
+  this.state = state;
+}
+
+function addBookToLibrary(bookObject) {
+  return new Book(bookObject);
+}
+
+const currentSession = {};
+const userSettings = {};
+
+const domItem = {
   topbar: document.querySelector("#topbar"),
   sidebar: document.querySelector("#sidebar"),
   backdrop: document.querySelector("#backdrop"),
   menuButton: document.querySelector("#menuButton"),
-  sidebarButton: document.querySelector("#sidebarButton"),
+  sidebarClose: document.querySelector("#sidebarClose"),
 };
-
+const domItemsAll = {
+  stateItems: document.querySelectorAll("[data-state]"),
+};
 const domFunctions = {
-  switchState(domItem) {
-    domItem.dataset.state =
-      domItem.dataset.state === "deactive" ? "active" : "deactive";
+  switchState(item) {
+    item.dataset.state =
+      item.dataset.state === "deactive" ? "active" : "deactive";
+  },
+  deactiveAll() {
+    domItemsAll.stateItems.forEach((item) => {
+      item.dataset.state = "deactive";
+    });
+  },
+  activateItem(...arg) {
+    arg.forEach((item) => {
+      item.dataset.state = "active";
+    });
   },
 };
 
-domItems.backdrop.addEventListener("click", () => {
-  domFunctions.switchState(domItems.backdrop);
-  domFunctions.switchState(domItems.sidebar);
+domItem.backdrop.addEventListener("click", () => {
+  domFunctions.deactiveAll();
 });
-domItems.menuButton.addEventListener("click", () => {
-  domFunctions.switchState(domItems.backdrop);
-  domFunctions.switchState(domItems.sidebar);
+domItem.menuButton.addEventListener("click", () => {
+  domFunctions.switchState(domItem.backdrop);
+  domFunctions.switchState(domItem.sidebar);
 });
-domItems.sidebarButton.addEventListener("click", () => {
-  domFunctions.switchState(domItems.backdrop);
-  domFunctions.switchState(domItems.sidebar);
+domItem.sidebarClose.addEventListener("click", () => {
+  domFunctions.deactiveAll();
 });
